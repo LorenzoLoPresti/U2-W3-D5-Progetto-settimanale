217c48b8082fe3e85d613e18fb661253be088525
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-01-27 15:20:42 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+let cardContainerReference = document.getElementById("card-section");
+
 const findMusic = function (keyword, index) {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${keyword}`)
     .then(function (response) {
@@ -9,13 +11,13 @@ const findMusic = function (keyword, index) {
       console.log(element.data);
       for (let i = 0; i < index; i++) {
         {
-          let cardContainerReference = document.getElementById("card-section");
           cardContainerReference.innerHTML =
             cardContainerReference.innerHTML +
-            `<div class="col"> <div class="card" style="width: 18rem">
+            `<div class="col my-3"> <div class="card" style="width: 18rem">
                 <img src="${element.data[i].album.cover_medium}" class="card-img-top" alt="..." />
                 <div class="card-body">
                   <h5 class="card-title">${element.data[i].title}</h5>
+                  <h6>${element.data[i].album.title}</h6>
                   <p class="card-text">
                     Some quick example text to build on the card title and make up
                     the bulk of the card's content.
@@ -28,14 +30,13 @@ const findMusic = function (keyword, index) {
       }
     });
 };
-findMusic("fleetwood mac", 26);
+findMusic("artic monkeys", 4);
 
 const btnReference = document.getElementById("btn");
 const input = document.getElementById("input");
 console.log(input);
 
-btnReference.onclick =
-  ("click",
-  function () {
-    console.log("ciao");
-  });
+btnReference.addEventListener("click", function () {
+  cardContainerReference.innerHTML = "";
+  findMusic(input.value, 6);
+});
